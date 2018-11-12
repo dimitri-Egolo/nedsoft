854f237f25e41e3c9aad3893ccf729df5fc8d70f
nedimitri <nedimitri@gmail.com>
2018-11-12 21:43:17 +0100

    adding media files config

--- nedsoft/prod_settings.py
+++ nedsoft/prod_settings.py
@@ -28,3 +28,4 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'nedsoft.storage_backends.MediaStorage'
